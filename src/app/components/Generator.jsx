a2338f835e13b42b4a8504a143fc5aa56aaef7f2
Bonniezilla <bonnieevil86@gmail.com>
2023-06-30 14:18:35 -0300

    Deixando mais minimalista e mais estável

--- src/app/components/Generator.jsx
+++ src/app/components/Generator.jsx
@@ -4,24 +4,19 @@ import { useEffect, useState } from "react";
 
 export default function Generator() {
     const [passwords, setPasswords] = useState([]);
-    const [passwordConfig, setPasswordConfig] = useState({ mount: 4, size: 8});
-    
+    const [passwordConfig, setPasswordConfig] = useState({ size: 8 });
+
     const handleChange = (event) => {
-        if (event.target.key = 'length-input') {
+        if (event.target.id === 'size-input') {
             setPasswordConfig(config => ({
                 ...config,
-                size: event.target.value
-            }));
-        } else if (event.target.key = 'mount-input') {
-            setPasswordConfig(config => ({
-                ...config,
-                mount: event.target.value
+                size: parseInt(event.target.value)
             }));
         }
     }
 
     useEffect(() => {
-        getPasswords(passwordConfig.mount, passwordConfig.length)
+        getPasswords(passwordConfig.size);
     }, [passwordConfig])
 
     function createPassword(length) {
@@ -36,48 +31,48 @@ export default function Generator() {
         const allChars = '!"#$%&\'()*+,-./0123456789:;<=>?@[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
         let password = '';
 
-        if (length >= 4 && length <= 35) {
-            for (let i = 1; i <= length; i++) {
-                password += getRandomChar();
-            }
-        } else {
-            for (let i = 1; i <= 4; i++) {
-                password += getRandomChar();
-            }
+        for (let i = 1; i <= length; i++) {
+            password += getRandomChar();
         }
         return password;
-        
+
     }
 
-    function getPasswords(passwordsNumber = 4, passwordsLength = 8) {
+    function getPasswords(passwordsLength = 8) {
         let passwordsArray = [];
 
-        for (let i = 1; i <= passwordsNumber; i++) {
-            passwordsArray.push(createPassword(passwordsLength));
+        if (passwordsLength <= 35 && passwordsLength >= 5) {
+            for (let i = 1; i <= 4; i++) {
+                passwordsArray.push(createPassword(passwordsLength));
+            }
+            setPasswords(passwordsArray);
         }
 
-        setPasswords(passwordsArray);
     }
 
     return (
-        <section className="h-5/6 p-6 w-5/6 flex flex-col items-center justify-around
-        ">
-            <span className="font-bold text-center gap-6 flex flex-col justify-center 
+        <section className="h-5/6 p-6 w-5/6 flex flex-col items-center justify-center
+        gap-4">
+            <span className="font-bold text-center gap-2 flex flex-col justify-center 
             items-center">
                 <h1 className="text-5xl animate-text-entry text-white
                 "><span className="text-emerald-500">Peace</span> Word</h1>
                 <h2 className="text-white text-3xl font-bold animate-text-entry-invert
                 ">Never use weak <span className="text-emerald-500">passwords </span>again.</h2>
             </span>
-            <div className="flex flex-nowrap">
-                <button onClick={() => getPasswords(passwordConfig.mount, passwordConfig.length)}
+            <div className="flex flex-nowrap items-center justify-center gap-6">
+                <button onClick={() => getPasswords(passwordConfig.mount, passwordConfig.size)}
                     className="text-white bg-emerald-500 p-2 border-2
                 border-solid border-white rounded-lg"
                 >New Password</button>
-                <input type="number" placeholder="Password mount"
-                    className="config-input" onChange={handleChange} key={'mount-input'}/>
-                <input type="number" placeholder="Password size"
-                    className="config-input" onChange={handleChange} key={'length-input'}/>
+                <div className="flex flex-col">
+                    <input type="range" min="5" max="35" defaultValue={passwordConfig.size}
+                        className="size-input" onChange={handleChange} id={'size-input'} />
+                    <span className="text-white flex flex-nowrap font-bold
+                    items-center justify-center">
+                    Size: <span className="text-emerald-500">{passwordConfig.size}</span>
+                    </span>
+                </div>
             </div>
             <div className="md:grid-cols-1 md:p-6 
             lg:grid-cols-2 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Generator from './components/Generator';
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center
+    <main className="flex h-full min-h-screen w-screen flex-col items-center justify-center
     bg-slate-900">
       <Generator />   
     </main>
