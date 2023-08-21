f597ef4127432d54a76f1ed693f1295372e7e46d
Bonniezilla <bonnieevil86@gmail.com>
2023-08-21 10:25:33 -0300

    Estilizando o range

--- src/app/components/Generator.jsx
+++ src/app/components/Generator.jsx
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react";
 
 export default function Generator(props) {
     const [passwords, setPasswords] = useState([]);
-    const [passwordConfig, setPasswordConfig] = useState({ 
-        number: props.passwordsNumber >= 1 ? props.passwordsNumber : 4 
-        , size: 8 });
+    const [passwordConfig, setPasswordConfig] = useState({
+        number: props.passwordsNumber >= 1 ? props.passwordsNumber : 4
+        , size: 8
+    });
 
     const handleChange = (event) => {
         if (event.target.id === 'size-input') {
@@ -67,24 +68,26 @@ export default function Generator(props) {
                 <h2 className="text-white text-3xl font-bold animate-text-entry-invert
                 ">Never use weak <span className="text-emerald-500">passwords </span>again.</h2>
             </span>
-            <div className="flex flex-nowrap max-sm:flex-col items-center justify-center gap-6
-            max-sm:w-full max-sm:px-4">
+            <div className="flex flex-nowrap max-lg:flex-col justify-around items-center
+            max-lg:w-full max-lg:px-4 w-4/12 h-32">
                 <button onClick={() => getPasswords(passwordConfig.size)}
                     className="text-white bg-emerald-500 p-2 border-2
                 border-solid border-white rounded-lg"
                 >New Password</button>
-                <div className="flex flex-col w-full">
-                    <input type="range" min="5" max="35" defaultValue={passwordConfig.size}
-                        className="size-input" onChange={handleChange} id={'size-input'} />
-                    <span className="text-white flex flex-nowrap font-bold
-                    items-center justify-center">
-                        Size: <span className="text-emerald-500">{passwordConfig.size}</span>
-                    </span>
+                <div className="flex w-6/12 h-12 items-center justify-around">
+                    <div className="flex flex-col w-full h-full">
+                        <input type="range" min="5" max="35" defaultValue={passwordConfig.size}
+                            className="size-input" onChange={handleChange} id={'size-input'} />
+                        <span className="text-white flex flex-nowrap font-bold
+                        items-center justify-center h-full w-full">
+                            Size: <span className="text-emerald-500">{passwordConfig.size}</span>
+                        </span>
+                    </div>
                 </div>
             </div>
             <div className={`max-sm:px-0 
             md:p-6 grid items-center
-        gap-6 w-full h-3/6 ${ passwordConfig.number <= 1 ? "grid-cols-1" : "lg:grid-cols-2 md:grid-cols-1"}`}>
+        gap-6 w-full h-3/6 ${passwordConfig.number <= 1 ? "grid-cols-1" : "lg:grid-cols-2 md:grid-cols-1"}`}>
                 {passwords.map(password => (
                     <input className="password-input"
                         type="text"
