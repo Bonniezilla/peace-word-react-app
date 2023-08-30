49c0f3a970001bc7bab2e5aa7f1f2f3ea753bc5b
Bonniezilla <bonnieevil86@gmail.com>
2023-08-30 09:40:44 -0300

    Organizando e separando com componentes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Generator from './components/Generator'; 
+import Header from './components/Header';
 
 export default function Home() {
   return (
-    <main className="flex h-full min-h-screen w-screen flex-col items-center justify-center
-    bg-slate-900">
+    <main className="flex h-screen w-screen flex-col items-center justify-center"
+    >
+      <Header />
       <Generator passwordsNumber="4"/>   
     </main>
   )
