4ff70e80856417d89a7105638d919141c1110f42
Pedro Peixoto <bonnieevil86@gmail.com>
2025-08-02 20:24:53 -0300

    Arrumando o fetch

--- src/app/components/PasswordViewer.tsx
+++ src/app/components/PasswordViewer.tsx
@@ -4,39 +4,19 @@ import React, { useEffect } from "react";
 
 const PasswordViewer = () => {
     const [passwordsData, setPasswordsData] = React.useState([]);
-    const [error, setError] = React.useState(null);
+    const apiURL = process.env.NEXT_PUBLIC_PEACEWORD_API;
+    console.log(apiURL + "/users/1/passwords");
+
 
     useEffect(() => {
-        const fetchPasswords = async () => {
-            try {
-                const response = await fetch(process.env.PEACEWORD_API + "/users/1/passwords");
-                if (!response.ok) {
-                    throw new Error("Failed to fetch passwords");
-                }
-                const data = await response.json();
-                setPasswordsData(data);
-            } catch (error: any) {
-                setError(error.message || "Unknown error");
-            }
-        };
-        fetchPasswords();
+        fetch(apiURL + "/users/1/passwords")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setPasswordsData(data)
+        })
+        .catch((error) => console.error("Error fetching passwords:", error));
     }, []);
-
-    if (error) {
-        return (
-            <div className="w-11/12 p-12 h-full justify-self-center bg-red-500/30 flex items-center justify-center">
-                <h1 className="text-white font-bold text-2xl">Error: {error}</h1>
-            </div>
-        );
-    }
-    
-    if(!passwordsData || passwordsData.length === 0) { 
-        return (
-            <div className="w-11/12 p-12 h-full justify-self-center bg-yellow-500/30 flex items-center justify-center">
-                <h1 className="text-white font-bold text-2xl">No passwords found</h1>
-            </div>
-        );
-    };
 
     return (
         <div className="w-11/12 p-12 h-full justify-self-center bg-emerald-950/30 flex items-center justify-center">
