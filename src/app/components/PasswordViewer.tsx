d744382e596ac019c3a12fc7f4e6aa8a9a49b23f
Pedro Peixoto <bonnieevil86@gmail.com>
2025-08-04 18:06:12 -0300

    Adding loading and error message to fetch passwords

--- src/app/components/PasswordViewer.tsx
+++ src/app/components/PasswordViewer.tsx
@@ -5,8 +5,10 @@ import React, { useEffect } from "react";
 const PasswordViewer = () => {
     const [passwordsData, setPasswordsData] = React.useState([]);
     const apiURL = process.env.NEXT_PUBLIC_PEACEWORD_API;
-    console.log(apiURL + "/users/1/passwords");
 
+    const [error, setError] = React.useState(null);
+    
+    const [loading, setLoading] = React.useState(true);
 
     useEffect(() => {
         fetch(apiURL + "/users/1/passwords", {
@@ -16,16 +18,28 @@ const PasswordViewer = () => {
         })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            setPasswordsData(data)
+            setPasswordsData(data);
+            setLoading(false);
         })
-        .catch((error) => console.error("Error fetching passwords:", error));
+        .catch((error) => {
+            console.error("Error fetching passwords:", error)
+            setError(error);
+            setLoading(false);});
     }, []);
 
     return (
         <div className="w-11/12 p-12 h-full justify-self-center bg-emerald-950/30 flex items-center justify-center">
             <ul className="flex flex-wrap gap-6">
-                {passwordsData.map((password) => (
+                { loading ? (
+                    <div className="w-96 bg-slate-800 flex flex-col gap-2 items-center justify-center p-8 rounded-2xl">
+                        <p className="text-white text-2xl font-bold">Loading...</p>
+                    </div>
+                ): error ? (
+                    <div className="w-96 bg-slate-800 flex flex-col gap-2 items-center justify-center p-8 rounded-2xl">
+                        <p className="text-red-500 text-2xl">Error fetching passwords: <span className="font-bold">{error.message}</span></p>
+                    </div>
+                ):
+                passwordsData.map((password) => (
                     <li key={password.id} className="w-96 bg-emerald-600 flex flex-col gap-2 items-center justify-center p-8 rounded-2xl">
                         <h1 className="text-white font-bold text-3xl">{password.password}</h1>
                         <h2 className="text-white text-1xl"><span className="font-bold">Category: </span>{password.category}</h2>
